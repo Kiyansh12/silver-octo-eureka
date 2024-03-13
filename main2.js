1bfe66a2b2cb310f26ee20082165593cb4bf4da3
Kiyansh12 <142411681+Kiyansh12@users.noreply.github.com>
2024-03-13 18:36:42 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,14 @@
+function pets()
+{
+    window.location = "pets.html";
+}
+
+function bottles()
+{
+    window.location = "in4dex.html";
+}
+
+function bowls()
+{
+    window.location = "in3dex.html";
+}
